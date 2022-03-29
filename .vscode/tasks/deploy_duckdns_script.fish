c4f0c3613cced934065f092965f29760a8ed885b
fstemarie <fstemarie@gmail.com>
2022-03-29 02:06:43 -0400

    Added task to deploy duckdns script

---
+++ .vscode/tasks/deploy_duckdns_script.fish
@@ -0,0 +1,22 @@
+#! /usr/bin/fish
+# This script will deploy the backup scripts.
+
+set dst /data/automation/duckdns
+set s (basename (status current-filename))
+
+if test ! -d "$dst"
+    echo "$s -- Creating non existing destination"
+    mkdir -p $dst
+    if test $status -ne 0
+        echo "$s -- Error while creating destination. Unable to proceed..."
+        exit
+    end
+end
+
+echo "$s -- Copying duckdns script to destination"
+cp --remove-destination "./src/duckdns/duckdns.fish" "$dst"
+if test $status -ne 0
+    echo "$s -- Error whlie copying script"
+    exit
+end
+echo "$s -- Script copied successfully"
