3cc7b4aeaf8acb5cb8774c82bdd3c19b057e5880
fstemarie <fstemarie@gmail.com>
2022-03-28 20:33:08 -0400

    Changed to user borg

--- src/backup/home.bkp.fish
+++ src/backup/home.bkp.fish
@@ -1,36 +1,32 @@
 #! /usr/bin/fish
 
-set nb_max_backups 5
 set src /home/francois
-set dst /l/backup/raktar/home
-set arch $dst"/home."(date +%Y%m%dT%H%M%S | tr -d :-)".tgz"
+set BORG_REPO /l/backup/raktar/borg
 
-# if the source folder doesn't exist, then there is nothing to backup
-if test ! -d $src
-    logger -t home.bkp.fish "Source folder does not exist"
-    echo "home.bkp.fish -- Source folder does not exist"
+logger -t home.bkp.fish "Creating borg archive"
+echo "home.bkp.fish -- Creating borg archive"
+borg create --list --filter=AME --progress --compression lzma,9 -s \
+    --exclude '/home/francois/.cache' \
+    --exclude '/home/francois/.vscode*' \
+    --exclude /home/francois/development \
+    $BORG_REPO::home.{now} $src
+
+if test $status -ne 0
+    logger -t home.bkp.fish "There was an error while creating the Borg archive"
+    echo "home.bkp.fish -- There was an error while creating the Borg archive"
     exit
 end
+echo "home.bkp.fish -- Borg archive created successfully"
 
-# if the destination folder does not exist, create it
-if test ! -d $dst
-    echo "home.bkp.fish -- Creating non-existent destination"
-    mkdir -p $dst
+
+logger -t home.bkp.fish "Pruning borg archive"
+echo "home.bkp.fish -- Pruning borg archive"
+borg prune -s --list --save-space --prefix home. \
+    --keep-daily 14 --keep-monthly 6 \
+    $BORG_REPO
+if test $status -ne 0
+    logger -t home.bkp.fish "Unable to prune Borg archives"
+    echo "home.bkp.fish -- Unable to prune Borg archives"
+    exit
 end
-
-echo "home.bkp.fish -- Creating archive"
-cd $src/..
-tar \
-    --exclude "francois/.cache" \
-    --exclude "francois/development" \
-    --exclude "francois/.vscode-*" \
-    -cvzf $arch francois
-
-set nb_backups (command ls -1trd $dst/home.*.tgz | wc -l)
-set nb_backups_todelete (math $nb_backups - $nb_max_backups)
-if test $nb_backups_todelete -gt 0
-    echo "home.bkp.fish -- Removing older archives"
-    command ls -1trd $dst/home.*.tgz \
-        | head -n$nb_backups_todelete \
-        | xargs rm -f
-end
+echo "home.bkp.fish -- Borg archives pruned successfully"
