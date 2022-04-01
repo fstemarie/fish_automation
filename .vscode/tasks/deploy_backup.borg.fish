7526bced0f7af9c1e67cba98537913964ba55aed
fstemarie <fstemarie@gmail.com>
2022-04-01 01:32:55 -0400

    Added script for backup.borg

---
+++ .vscode/tasks/deploy_backup.borg.fish
@@ -0,0 +1,22 @@
+#! /usr/bin/fish
+# This script will deploy the backup scripts.
+
+set dst /data/automation/backup.borg
+set s (basename (status current-filename))
+
+if test ! -d "$dst"
+    echo "$s -- Creating non existing destination"
+    mkdir -p $dst
+    if test $status -ne 0
+        echo "$s -- Error while creating destination. Unable to proceed..."
+        exit
+    end
+end
+
+echo "$s -- Copying backup scripts to destination"
+cp --remove-destination ./src/backup.borg/* $dst
+if test $status -ne 0
+    echo "$s -- Error while copying backup scripts"
+    exit
+end
+echo "$s -- Scripts copied successfully"
