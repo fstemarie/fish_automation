8e66bd8bb8d1547716e70286d22e3665621233df
fstemarie <14352349+fstemarie@users.noreply.github.com>
2022-07-31 18:01:49 -0400

    Added duckdns to daily script

--- src/daily.fish
+++ src/daily.fish
@@ -1,7 +1,8 @@
 #! /usr/bin/fish
 
 set backup "/data/automation/backup/"
-set scripts "$backup/restic/"{development,documents}".bkp.fish"
+set scripts "/data/automation/duckdns/duckdns.fish"
+set -a scripts "$backup/restic/"{development,documents}".bkp.fish"
 set -a scripts "$backup/tar/"{development,documents}".bkp.fish"
 
 for script in $scripts
