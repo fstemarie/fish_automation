e337c4070f90488078cc2724dd90b74b98ae6215
fstemarie <14352349+fstemarie@users.noreply.github.com>
2022-10-30 17:15:50 -0400

    Added scripts to weekly backups

--- src/weekly.fish
+++ src/weekly.fish
@@ -2,6 +2,7 @@
 
 set backup "/data/automation/backup"
 set scripts "$backup/tar/"{automation,config,jackett,qbittorrent}".bkp.fish"
+set -a scripts "$backup/restic/"{automation,config,containers,jackett,qbittorrent}".bkp.fish"
 
 for script in $scripts
     echo "Running: $script"
