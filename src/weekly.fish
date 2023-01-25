f1d689d9ee3d953216d92a3e262b15ede2a3ef0b
fstemarie <14352349+fstemarie@users.noreply.github.com>
2023-01-25 01:08:18 -0500

    removed jackett and qbittorrent restic scripts references

--- src/weekly.fish
+++ src/weekly.fish
@@ -6,8 +6,6 @@ set scripts "$backup/tar/"{automation,config,jackett,qbittorrent}".bkp.fish"
 $backup/restic/automation.bkp.fish
 $backup/restic/config.bkp.fish
 $backup/restic/containers.bkp.fish
-$backup/restic/jackett.bkp.fish
-$backup/restic/qbittorrent.bkp.fish
 restic prune
 
 for script in $scripts
