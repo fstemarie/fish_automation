f67256fca1851dad6575f55bd2a4c2c1c2729112
fstemarie <14352349+fstemarie@users.noreply.github.com>
2023-05-12 00:35:02 -0400

    Change domaine johnnywalker pour pegleg

--- src/duckdns.fish
+++ src/duckdns.fish
@@ -1,6 +1,14 @@
 #! /usr/bin/fish
-set domains falarie johnnywalker
+set domains falarie pegleg
 set token d05499d5-3208-4b85-8bf2-be3ebbdc3ec2
+
+echo "
+
+
+-------------------------------------
+ "(date -Ins)" 
+-------------------------------------
+" | tee -a $log
 
 for domain in $domains
 	set fulldomain $domain.duckdns.org
@@ -8,15 +16,15 @@ for domain in $domains
 	set response (curl -s -k -o - -- $refresh_url)
 
 	echo $refresh_url
-	echo $response
+	echo $response 
 	if test "$response" = "OK"
 		logger -t duckdns.fish "Domain $fulldomain was successfully refreshed"
-		echo "duckdns.fish -- Domain $fulldomain was successfully refreshed"
+		echo "duckdns.fish -- Domain $fulldomain was successfully refreshed" | tee -a $log
 	else if test "$response" = "KO"
 		logger -t duckdns.fish "There was an error while attempting to refresh $fulldomain"
-		echo "duckdns.fish -- There was an error while attempting to refresh $fulldomain"
+		echo "duckdns.fish -- There was an error while attempting to refresh $fulldomain" | tee -a $log
 	else
 		logger -t duckdns.fish "Something weird happened while attempting to refresh $fulldomain"
-		echo "duckdns.fish -- Something weird happened while attempting to refresh $fulldomain"
+		echo "duckdns.fish -- Something weird happened while attempting to refresh $fulldomain" | tee -a $log
 	end
 end
