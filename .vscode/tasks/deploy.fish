6d10c37d6373e28db09a70146f261bd96257ff14
fstemarie <14352349+fstemarie@users.noreply.github.com>
2025-01-16 22:56:44 -0500

    refactored the deployment script

--- .vscode/tasks/deploy.fish
+++ .vscode/tasks/deploy.fish
@@ -4,19 +4,23 @@
 set dst "/data/automation"
 set s (basename (status current-filename))
 
-if test ! -d "$dst"
-    echo "$s -- Creating non existing destination $dst"
-    mkdir -p "$dst"
+if test -d "$dst"
+    rm -fr "$dst"
     if test $status -ne 0
-        echo "$s -- Error while creating destination. Unable to proceed..."
+        echo "$s -- Error while deleting destination. Unable to proceed..."
         exit 1
     end
 end
 
-echo "$s -- Copying backup scripts to destination"
-rm -fr "$dst"
+echo "$s -- Creating non existing destination $dst"
 mkdir -p "$dst"
-cp -R --remove-destination ./src/* "$dst"
+if test $status -ne 0
+    echo "$s -- Error while creating destination. Unable to proceed..."
+    exit 1
+end
+
+echo "$s -- Copying backup scripts to destination"
+cp -R ./src/* "$dst"
 if test $status -ne 0
     echo "$s -- Error while copying backup scripts"
     exit 1
