f995f37df1a245495391cacfeee2be2d2adfe18e
fstemarie <14352349+fstemarie@users.noreply.github.com>
2025-05-03 16:06:45 -0400

    completely redone deployment script

--- .vscode/tasks/deploy.fish
+++ .vscode/tasks/deploy.fish
@@ -2,28 +2,40 @@
 # This script will deploy the backup scripts.
 
 set dst "/data/automation"
-set s (basename (status current-filename))
-
-if test -d "$dst"
-    rm -fr "$dst"
-    if test $status -ne 0
-        echo "$s -- Error while deleting destination. Unable to proceed..."
-        exit 1
-    end
-end
-
-echo "$s -- Creating non-existing destination $dst"
+echo "-- Copying scripts to $dst"
+rm -fr $dst
 mkdir -p "$dst"
+cp -R ./src/*.fish "$dst"
 if test $status -ne 0
-    echo "$s -- Error while creating destination. Unable to proceed..."
+    echo "-- Error while copying scripts"
     exit 1
 end
 
-echo "$s -- Copying backup scripts to destination"
-cp -R ./src/* "$dst"
+set dst "/data/automation/backup"
+echo "-- Copying docker update scripts to $dst"
+mkdir -p "$dst"
+cp -R ./src/backup/* "$dst"
 if test $status -ne 0
-    echo "$s -- Error while copying backup scripts"
+    echo "-- Error while copying backup scripts"
     exit 1
 end
 
-echo "$s -- Scripts copied successfully"
+set dst "/data/automation/docker"
+echo "-- Copying backup scripts to $dst"
+mkdir -p "$dst"
+cp -R ./src/docker/* "$dst"
+if test $status -ne 0
+    echo "-- Error while copying backup scripts"
+    exit 1
+end
+
+set dst "/data/config/logrotate"
+echo "-- Copying logrotate config to $dst"
+mkdir -p "$dst"
+cp -R ./src/logrotate/* "$dst"
+if test $status -ne 0
+    echo "-- Error while copying backup scripts"
+    exit 1
+end
+
+echo "-- Scripts copied successfully"
